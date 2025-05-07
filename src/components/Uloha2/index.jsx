c667b0da13522b7b39b60f5654821d06da43dceb
Katarina Packova <katarina.packova@gmail.com>
2025-05-07 09:51:54 +0200

    Updated

--- src/components/Uloha2/index.jsx
+++ src/components/Uloha2/index.jsx
@@ -5,5 +5,13 @@ Zadání: Po najetí myši, `onMouseEnter` na obrázek zobrazte alert s textem �
 */
 
 export const Uloha2 = () => {
-  return <img src={hradUrl} width={60} height={60} alt="hrad" />;
+  return (
+    <img
+      onMouseEnter={() => alert('„Na hradě je myš!“')}
+      src={hradUrl}
+      width={60}
+      height={60}
+      alt="hrad"
+    />
+  );
 };
